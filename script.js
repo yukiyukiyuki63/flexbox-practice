4e8ade94f4c248b5af90664f91a9b27714dd8952
yukiyukiyuki63 <callistemon.r.br@gmail.com>
2026-02-28 16:30:32 +0900

    Day11

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const btn = document.querySelector('.btn');
 const box = document.querySelector('.box');
-const menuBtn = document.querySelector('.menu-btn')
-const menu = document.querySelector('menu')
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
 
 btn.addEventListener('click',() => {
     box.classList.toggle('active');
 });
-menuBtn.addEventListener('click',()=>){
+
+menuBtn.addEventListener('click',() => {
     menu.classList.toggle('active');
-};
+});
